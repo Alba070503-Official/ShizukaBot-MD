7cf2c167ae7697cc17b26643bc949c5e7418ea49
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-06-06 22:38:18 -0400

    Update prueba4.js

--- plugins/prueba4.js
+++ plugins/prueba4.js
@@ -1,94 +1,210 @@
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-  let totalusr = Object.keys(global.db.data.users).length;
-  let _uptime = process.uptime() * 1000;
-  let uptime = clockString(_uptime);
-  let username = conn.getName(m.sender);
-  let locale = 'es';
-  let d = new Date(new Date + 3600000);
-  let time = d.toLocaleTimeString(locale, {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
-  });
+import { promises } from 'fs'
+import { join } from 'path'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
 
-  await conn.reply(m.chat, '⏱ _Cargando Menu_', m);
-  m.react("✨");
-
-  let sbot = conn.user.jid == global.conn.user.jid ? "" : `Sᴏʏ Sᴜʙʙᴏᴛ Dᴇ:  Wa.me/${global.conn.user.jid.split`@`[0]}`;
-
-  global.fcontact = {
-    key: {
-      fromMe: false,
-      participant: `0@s.whatsapp.net`,
-      remoteJid: "status@broadcast",
-    },
-    message: {
-      contactMessage: {
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${username}\nitem1.TEL;waid=${m.sender.split("@")[0]}:${m.sender.split("@")[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
-      },
-    },
-  };
-let menu = '.'
-  let menu2 = `
-*╭─╮︹︹︹⊹︹︹︹⊹︹︹︹⊹*
-*┆  ${username}*
-*╰─ ⊹꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦꒷꒦⊹*
-
-╭─╮︹︹︹⊹︹︹︹⊹︹︹︹⊹─╮
-┆   *🌹 ɪɴғᴏ  ᴅᴇ ʟᴀ ʙᴏᴛ 🍒*
-┝╾─────────────────╯
-┆⏱ *𝘓𝘭𝘦𝘷𝘰 𝘈𝘤𝘵𝘪𝘷o: ${uptime}*
-┆Este menu esta en fase prueba 
-╰ׄ┄ׅׄ─ׅׄ┄ׅׄ─ׂׅ┄ׅׄ─ׂׅ┄ׅׄ─ׅׄ┄ׅׄ─ׂׅ┄ׅׄ─ׂׅ┄ׅׄ─ׅׄ─ׂׅ┄ׅׄ─ׂ╯`;
-
-  let listSections = [{
-    title: '',
-    rows: [
-      { title: "Menu Completo", rowId: `${usedPrefix}menu`, description: "Para ver todos los comandos" },
-      { title: "SerBot", rowId: `${usedPrefix}serbot --code`, description: "Para volverte subbot 🤖" },
-      { title: "Velocidad", rowId: `${usedPrefix}ping`, description: "Ver velocidad del bot" },
-      { title: "Play", rowId: `${usedPrefix}play`, description: "Para descargar música 🎧" },
-      { title: "Creador", rowId: `${usedPrefix}owner`, description: "Comunícate con mi creador ⚙️" }
-    ]
-  }];
-
-  let img = "https://i.ibb.co/wstbFdW/file.jpg"; // Ejemplo de una de las imágenes
-  let greeting = getGreeting();
-
-  await conn.sendMessage(m.chat, {
-    image: { url: img },
-    caption: greeting + menu2,
-    footer: 'Click Aquí',
-    templateButtons: [
-      { index: 1, quickReplyButton: { displayText: 'Menu Completo', id: `${usedPrefix}menu` } },
-      { index: 2, quickReplyButton: { displayText: 'SerBot', id: `${usedPrefix}serbot --code` } },
-      { index: 3, quickReplyButton: { displayText: 'Velocidad', id: `${usedPrefix}ping` } },
-      { index: 4, quickReplyButton: { displayText: 'Play', id: `${usedPrefix}play` } },
-      { index: 5, quickReplyButton: { displayText: 'Creador', id: `${usedPrefix}owner` } }
-    ]
-  });
-};
-
-handler.command = ["menu"];
-export default handler;
-
-function clockString(ms) {
-  const h = Math.floor(ms / 3600000);
-  const m = Math.floor(ms / 60000) % 60;
-  const s = Math.floor(ms / 1000) % 60;
-  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(":");
+let tags = {
+  'main': 'INFO',
+  'game': 'JUEGOS',
+  'serbot': 'SUB BOTS',
+  'rpg': 'ECONOMÍA',
+  'rg': 'REGISTRO',
+  'downloader': 'DESCARGAS',
+  'marker': 'LOGO - MAKER',
+  'nable': 'ACTIVADORES',
+  'group': 'GRUPOS',
+  'search': 'BUSCADOR',
+  'img': 'IMÁGENES',
+  'tools': 'HERRAMIENTAS',
+  'fun': 'DIVERCIÓN',
+  'audio': 'EFECTO DE AUDIOS', 
+  'sticker': 'STICKERS',
+  'nsfw': 'NSFW',
+  'owner': 'CREADOR',
+  'advanced': 'AVANZADO',
 }
 
-function getGreeting() {
-  let hour = new Date().getHours();
-  switch (true) {
-    case (hour >= 0 && hour < 6):
-      return 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌙';
-    case (hour >= 6 && hour < 12):
-      return 'Bᴜᴇɴᴏs Dɪᴀs 🌅';
-    case (hour >= 12 && hour < 18):
-      return 'Bᴜᴇɴᴀs Tᴀʀᴅᴇs 🌇';
-    default:
-      return 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌙';
+const defaultMenu = {
+  before: `
+ > IGNA BOT FUTURE
+
+╭━━━━━━━∙⋆⋅⋆∙━━━━━━━━╮
+➤📝 *Nombre* : %name
+➤🪙 *Euros* : %limit
+➤🤖 *User* : %taguser
+➤📈 *Nivel* : %level
+➤⭐ *XP* : %totalexp
+╰━━━━━━━∙⋆⋅⋆∙━━━━━━━━╯
+
+╭━━━━━━━∙⋆⋅⋆∙━━━━━━━━╮
+➤🗣️ *Creador* : Daniel 🇦🇱
+➤📲 *Número* : Wa.me/51955918117
+➤⌛ *Tiempo* : %uptime
+╰━━━━━━━∙⋆⋅⋆∙━━━━━━━━╯
+
+%readmore
+`.trimStart(),
+  header: '`MENU X %category`\n\n╭━━━━━━━∙⋆⋅⋆∙━━━━━━━━╮',
+  body: '➤ *%cmd*\n',
+  footer: '╰━━━━━━━∙⋆⋅⋆∙━━━━━━━━╯\n',
+  after: '',
+}
+
+let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
+ try {
+    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+    let { exp, star, level } = global.db.data.users[m.sender]
+    let { min, xp, max } = xpRange(level, global.multiplier)
+    let name = await conn.getName(m.sender)
+    let d = new Date(new Date + 3600000)
+    let locale = 'es'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).format(d)
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+    let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+      return {
+        help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
+        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+        prefix: 'customPrefix' in plugin,
+        star: plugin.star,
+        premium: plugin.premium,
+        enabled: !plugin.disabled,
+      }
+    })
+    for (let plugin of help)
+      if (plugin && 'tags' in plugin)
+        for (let tag of plugin.tags)
+          if (!(tag in tags) && tag) tags[tag] = tag
+    conn.menu = conn.menu ? conn.menu : {}
+    let before = conn.menu.before || defaultMenu.before
+    let header = conn.menu.header || defaultMenu.header
+    let body = conn.menu.body || defaultMenu.body
+    let footer = conn.menu.footer || defaultMenu.footer
+    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : ``) + defaultMenu.after
+    let _text = [
+      before,
+      ...Object.keys(tags).map(tag => {
+        return header.replace(/%category/g, tags[tag]) + '\n' + [
+          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+            return menu.help.map(help => {
+              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
+                .replace(/%isstar/g, menu.star ? '˄' : '')
+                .replace(/%isPremium/g, menu.premium ? '˄' : '')
+                .trim()
+            }).join('\n')
+          }),
+          footer
+        ].join('\n')
+      }),
+      after
+    ].join('\n')
+    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+    let replace = {
+      '%': '%',
+      p: _p, uptime, muptime,
+      taguser: '@' + m.sender.split("@s.whatsapp.net")[0],
+      wasp: '@0',
+      me: conn.getName(conn.user.jid),
+      npmname: _package.name,
+      version: _package.version,
+      npmdesc: _package.description,
+      npmmain: _package.main,
+      author: _package.author.name,
+      license: _package.license,
+      exp: exp - min,
+      maxexp: xp,
+      totalexp: exp,
+      xp4levelup: max - exp,
+      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
+      level, star, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg,
+      readmore: readMore
+    }
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+    
+    let pp = 'https://telegra.ph/file/4c3e4b782c82511b3874d.mp4'
+    let pp2 = 'https://telegra.ph/file/d8c5e18ab0cfc10511f63.mp4'
+    let pp3 = 'https://telegra.ph/file/96e471a87971e2fb4955f.mp4'
+    let pp4 = 'https://telegra.ph/file/09b920486c3c291f5a9e6.mp4'
+    m.react('💻')
+   
+  //  conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: m })
+let listSections = []    
+listSections.push({
+title: '',
+rows: [{ header: "Menu Completo", title: "", id: `.menu`, description: `Para ver todos los comandos\n` }, { header: "SudBot", title: "", id: `.serbot --code`, description: `Para volverte sudbot 🤖\n` },
+{ header: "Velocidad", title: "", id: `.ping`, description: `Ver velocidad del bot\n` },
+{ header: "Play", title: "", id: `.play`, description: `Para descargar musica 🎧\n` },
+{ header: "creador", title: "", id: `.owner`, description: `comunicate con mi creador ⚙️` }
+]})
+
+  let img = "https://i.ibb.co/wstbFdW/file.jpg";
+  let img2 = "https://i.ibb.co/gwm5mXm/file.png";
+  let img3 = "https://i.ibb.co/0s8V5FM/file.png";
+  let img4 = "https://i.ibb.co/d6svLy0/file.jpg";
+  let img5 = "https://telegra.ph/file/2885253414cbf7b0878f3.jpg";
+  let img6 = "https://telegra.ph/file/ce57807eed899516484c7.jpg";
+  let img7 = "https://telegra.ph/file/63fd2e414b81fb8161d3a.jpg";
+  let img8 = "https://telegra.ph/file/3594f35921ce75d5c5e16.jpg";
+  let img9 = "https://telegra.ph/file/4f89789a5b1f874622ffd.jpg";
+  let img10 = "https://i.ibb.co/SKm0T9S/file.jpg";
+  let img11 = "https://telegra.ph/file/20633f54b3d054c95cd58.jpg";
+
+  
+  await conn.sendList(m.chat, menu, menu2, `Click Aqui`, [img, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11].getRandom(), listSections,);
+};
+   
+await conn.sendList(m.chat, '👋🏻 Hola¡! Bienvenido A Mi Sub Menú\n\n*Creador:* Alba070503\n*Versión:* 1.0.0\n\n💮 si hay algún error puedes contactarme, usa el comando: #owner\n\nGracias¡! 🔴', null, `Selecione la opción correcta (⁠・⁠∀⁠・⁠)`, [img, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11].getRandom(), listSections,);
+, { mentions: [m.sender]}, {quoted: m})
+  } catch (e) {
+    conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
+    throw e
   }
 }
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = ['menu', 'help', 'menú'] 
+handler.register = true 
+export default handler
+
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+      }
