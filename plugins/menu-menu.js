18efa28716b9a81de0c0200da1acad1248b4a8ac
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-06-07 08:01:01 -0400

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -612,7 +612,7 @@ ${readMore}
 
 ShizukaBot-MD|| Alba070503`.trim()
 //conn.sendFile(m.chat, pp, 'lp.jpg', menu, m, false, { contextInfo: { mentionedJid }})
-let img = await (await fetch('https://telegra.ph/file/b7edf2026656718c1f0f7.jpg')).buffer()  
+let img = await (await fetch('https://telegra.ph/file/c9031cb590eb25e0d81a6.jpg')).buffer()  
 await conn.sendMessage(m.chat, {
 text: menu,
 contextInfo: { 
