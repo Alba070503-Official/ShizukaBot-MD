360466ccfcbf99bca25c52b8d8ef941c96a43ede
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-06-12 23:17:58 -0400

    Update propietario(a)-update.js

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -1,37 +1,41 @@
-import { execSync } from 'child_process';
+import { execSync } from 'child_process'
 
-const handler = async (m, { conn, text }) => {
+var handler = async (m, { conn, text }) => {
+m.react('🚀') 
 try {
 const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
 let messager = stdout.toString()
-if (messager.includes('Already up to date.')) messager = `${lenguajeGB.smsAvisoIIG()} 𝙔𝘼 𝙀𝙎𝙏𝘼 𝘼𝘾𝙏𝙐𝘼𝙇𝙄𝙕𝘼𝘿𝙊 𝘼 𝙇𝘼 𝙑𝙀𝙍𝙎𝙄𝙊́𝙉 𝙍𝙀𝘾𝙄𝙀𝙉𝙏𝙀.`
-if (messager.includes('Updating')) messager = `${lenguajeGB.smsAvisoEG()}` + stdout.toString()
-conn.reply(m.chat, messager, m);
-} catch {      
-try {    
-const status = execSync('git status --porcelain');
+if (messager.includes('Already up to date.')) messager = '╰⊱🧿⊱ *𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗖𝗜𝗢́𝗡* ⊱🧿⊱╮\n\n*𝖸𝖠 𝖤𝖲𝖳𝖮𝖸 𝖠𝖢𝖳𝖴𝖠𝖫𝖨𝖹𝖠𝖣𝖠 𝖠 𝖫𝖠 𝖵𝖤𝖱𝖲𝖨𝖮́𝖭 𝖬𝖠𝖲 𝖱𝖤𝖢𝖨𝖤𝖭𝖳𝖤.*'
+if (messager.includes('Updating')) messager = '✅️ *Actualizacion Exitosa.*\n\n' + stdout.toString()
+conn.reply(m.chat, messager, m, fake,)
+} catch { 
+try {
+const status = execSync('git status --porcelain')
 if (status.length > 0) {
-const conflictedFiles = status
-.toString()
-.split('\n')
-.filter(line => line.trim() !== '')
-.map(line => {
-if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('GataBotSession/') || line.includes('npm-debug.log')) {
-return null;
+const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
+if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('sessions/') || line.includes('npm-debug.log')) {
+return null
 }
-return '*→ ' + line.slice(3) + '*'})
-.filter(Boolean);
+return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
 if (conflictedFiles.length > 0) {
-const errorMessage = `${lenguajeGB.smsAvisoFG()} > *Se han encontrado cambios locales en los archivos del bot que entran en conficto con las nuevas actualizaciones del repositorio. para actualizar, reinstalar el bot o realizar las actualizaciones manualmente.*\n\n*\`ARCHIVO EN CONFLICTO :\`*\n\n${conflictedFiles.join('\n')}.*`
-await conn.reply(m.chat, errorMessage, m);  
-}}
-} catch (error) {
-console.error(error);
-if (error.message) {
-errorMessage2 += `\n${fg}` + error.message;
+const errorMessage = `🚩 *Se Han Hecho Cambios Locales En Archivos Del Bot Que Entran En Conflicto Con Las Actualizaciones Del Repositorio. Para Actualizar, Reinstala El Bot o Realiza Las Actualizaciones Manualmente*\n\nArchivos En Conflicto:\n\n${conflictedFiles.join('\n')}`
+await conn.reply(m.chat, errorMessage, m, canalesInfo,)
 }
-await m.reply(`${fg}`) 
-}}};
-handler.command = /^(update|actualizar|gitpull)$/i;
-handler.rowner = true;
-export default handler;
+}
+} catch (error) {
+console.error(error)
+let errorMessage2 = '📍 *Ocurrió Un Error.*'
+if (error.message) {
+errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
+}
+await conn.reply(m.chat, errorMessage2, m, canalesInfo,)
+}
+}
+
+}
+handler.help = ['update', 'actualizar']
+handler.tags = ['owner']
+handler.command = /^(update|actualizar|gitpull)$/i
+handler.rowner = true
+
+export default handler
