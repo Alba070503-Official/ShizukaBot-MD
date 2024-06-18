6928af2e75cce662cb14dd7123aaf9b9ad363e2b
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-18 13:29:17 -0500

    Actualizar descargas-pinterest.js

--- plugins/descargas-pinterest.js
+++ plugins/descargas-pinterest.js
@@ -2,7 +2,7 @@ import Scraper from "@SumiFX/Scraper"
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) return m.reply('👑 Ingresa un texto para realizar la búsqueda.')
 try {
-m.react(rwait)
+//m.react(rwait)
 let { dl_url } = await Scraper.pinterest(text)
 await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', null, m)
 m.react(done)
