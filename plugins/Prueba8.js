7ff947b3fa204b07811c9416828755dfb65c843d
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-06-17 22:24:59 -0400

    Create Prueba8.js

---
+++ plugins/Prueba8.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const res = await fetch('https://api.waifu.pics/sfw/waifu');
+  if (!res.ok) return await res.text();
+  const json = await res.json();
+  if (!json.url) await m.reply('✧ Ocurrió un error inesperado');
+//conn.sendFile(m.chat, json.url, 'error.jpg', `❀ 😻`, m);
+conn.sendButton(m.chat, `❀ *IMAGEN DE PICS*`, '> *Nota ⪼* No genere Spam con los botones o será Baneado.', json.url, [['🔄 SIGUIENTE', `${usedPrefix + command}`]], m)
+};
+handler.help = ['waifu'];
+handler.tags = ['anime'];
+handler.command = ['waifu'];
+handler.registrado = true;
+handler.group = true;
+export default handler;
