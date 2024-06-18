18f849a41629c7dd84f7a08bebf98b687de6384b
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-06-17 22:03:51 -0500

    SizukaBot-MD By @Alba070503

---
+++ plugins/buscador-ip.js
@@ -0,0 +1,45 @@
+// Código gracias a SoIz1 
+// Github: https://github.com/SoIz1
+//Adaptado por KatashiFukushima. Github: https://github.com/KatashiFukushima
+
+import axios from 'axios'
+
+let handler = async (m, { conn, text }) => {
+await m.reply("Buscando...")
+  if (!text) return conn.reply(m.chat, "Ingrese una dirección IP válida", m)
+
+  axios.get(`http://ip-api.com/json/${text}?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,mobile,hosting,query`).then ((res) => {
+    const data = res.data
+
+      if (String(data.status) !== "success") {
+        throw new Error(data.message || "Falló")
+      }
+    let ipsearch = `
+*IP INFO*
+
+IP : ${data.query}
+País : ${data.country}
+Código de País : ${data.countryCode}
+Provincia : ${data.regionName}
+Código de Provincia : ${data.region}
+Ciudad : ${data.city}
+Distrito : ${data.district}
+Código Postal : ${res.data.zip}
+Coordenadas : ${data.lat}, ${data.lon}
+Zona Horaria : ${data.timezone}
+ISP : ${data.isp}
+Organización : ${data.org}
+AS : ${data.as}
+Mobile : ${data.mobile ? "Si" : "No"}
+Hosting : ${data.hosting ? "Si" : "No"}
+`.trim()
+
+conn.reply(m.chat, ipsearch, m)
+})
+}
+  
+handler.help = ['ip', 'ipcheck', 'ipcek'].map(v => v + ' <alamat ip>')
+handler.tags = ['tools']
+handler.command = /^(ip|ipcheck|ipcek)$/i
+handler.owner = true
+export default handler
