3fed69f41effb9e377d410fe5396f6ce04201639
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-06-23 14:13:00 -0400

    Update jadibot-lista.js

--- plugins/jadibot-lista.js
+++ plugins/jadibot-lista.js
@@ -1,18 +1,25 @@
+import ws from 'ws'
+import fetch from 'node-fetch'
 
-import ws from 'ws';
-async function handler(m, { usedPrefix }) {
-  let users = [...new Set([...global.conns.filter(conn => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map(conn => conn.user)])]
-  let b = users.map((v, i) => `_*${i + 1}.*_ *${v.name}*\nwa.me/${v.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}help`).join('\n')
- m.reply(` 
-≡ *Este es la lista de Sub-Bots*
-
-▢ *Total:* ${users.length} 
-
-${b}`) 
+async function handler(m, { conn: _envio, usedPrefix }) {
+const uniqueUsers = new Map()
   
-}
-handler.help = ['botlist']
-handler.tags = ['bebot']
-handler.command = ['listbot', 'listbots', 'bots', 'bebots', 'botlist'] 
+global.conns.forEach((conn) => {
+if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
+uniqueUsers.set(conn.user.jid.replace(/[^0-9]/g, ''), conn.user)}})
 
+const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  ✩  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  ✩  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n└  ✩  *Nombre* : ${user.name || 'Ai Hoshino'}\n`
+  ).join('\n')
+  
+const replyMessage = message.length === 0 ? "" : message
+const totalUsers = uniqueUsers.size;
+const responseMessage = `${` –  *S E R B O T  -  S U B B O T S*\n\n${replyMessage.trim()}`.trim()}`
+  
+let img = await (await fetch(`https://i.ibb.co/1dW0kGf/file.jpg`)).buffer()
+
+await _envio.sendFile(m.chat, img, 'thumbnail.jpg', responseMessage, m, false, { mentions: _envio.parseMention(responseMessage) })
+}
+handler.command = ['listjadibot', 'bots']
+handler.help = ['bots']
+handler.tags = ['serbot']
 export default handler
