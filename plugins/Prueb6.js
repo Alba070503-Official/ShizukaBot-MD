e4007dacd0da3dba602e23ee8dddf10761dd718f
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-07-04 19:08:22 -0400

    Update Prueb6.js

--- plugins/Prueb6.js
+++ plugins/Prueb6.js
@@ -22,7 +22,7 @@ await conn.reply(m.chat, '⏱ _Cargando Menu_', m )
 
   const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: img, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
-    };
+}
   let menu = ``;
 
   let menu2 = `
