fa2a33b2380dc4d1553c5fee7f2402cea75e703b
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-07-05 22:43:36 -0400

    Update convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -1,25 +1,35 @@
-/* 
-# Créditos a https://github.com/Undefined17
-•• @Azami19 ••
-*/
+/*Créditos a https://github.com/AzamiJs*/
+
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
-let handler = async (m) => {
+
+var handler = async (m) => {
+
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => gataImg.getRandom())
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
 let name = await conn.getName(who)
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw `${mg} ${mid.smsconvert10}`
+if (!mime) throw '*⚠️ RESPONDA A UNA IMAGEN*'
+m.react(done)
 let media = await q.download()
 let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
 let link = await (isTele ? uploadImage : uploadFile)(media)
-let caption = `🛑 ${mid.smsYT4}:\n${link}\n🥏 ${mid.smsconvert11}: ${media.length}\n🚀 ${mid.smsconvert12}: ${isTele ? '𝙉𝙊 𝙀𝙓𝙋𝙄𝙍𝘼' : '𝘿𝙀𝙎𝘾𝙊𝙉𝙊𝘾𝙄𝘿𝙊'}\n🔰 ${mid.smsconvert13}: ${await shortUrl(link)}`
-conn.reply(m.chat, caption, m, { contextInfo: {externalAdReply :{mediaUrl: md, mediaType: 2, title: wm, body: botdate, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link }}})}
+let info = ` *🗂️ ENLACE:*\n${link}\n
+*⚖️ TAMAÑO:*\n${media.length} bytes\n
+*🚀 EXPIRACION:*\n ${isTele ? '✅ NO EXPIRA' : '⚠️ DESCONOCIDO'}\n
+*🔰 ACORTADO:*\n${await shortUrl(link)}`
+
+conn.reply(m.chat, info, m, { contextInfo: { externalAdReply :{ mediaUrl: ig, mediaType: 2, title: wm, body: gatanina, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link}}})
+
+}
 handler.help = ['tourl']
-handler.tags = ['herramientas']
+handler.tags = ['transformador']
 handler.command = /^(tourl|upload)$/i
+
+handler.limit = true
+
 export default handler
 
 async function shortUrl(url) {
