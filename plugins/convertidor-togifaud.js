4dc5d43e4bfb98bad7c87ddb53212fb249f6b85e
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-06 21:34:42 -0500

    Actualizar convertidor-togifaud.js

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -1,11 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!m.quoted) return conn.reply(m.chat, `🤍 Responde a un *Video.*`, m, rcanal)
-conn.reply(m.chat, global.wait, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
+if (!m.quoted) return conn.reply(m.chat, `🤍 Responde a un *Video.*`, m)
 const q = m.quoted || m
 let mime = (q.msg || q).mimetype || ''
 if (!/(mp4)/.test(mime)) return conn.reply(m.chat, `🤍 Responde a un *Video.*`, m, rcanal)
