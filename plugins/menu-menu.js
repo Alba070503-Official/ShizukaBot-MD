e0752631d8dd7cf8da84568aac3d70800a6d510b
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-07-12 02:46:39 -0400

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -155,9 +155,11 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    
-    let pp = 'https://telegra.ph/file/19631e87aca87dbcacdc6.mp4'
-    await conn.sendFile(m.chat, pp, 'thumbnail.gif', text.trim(), m, null, rcanal)
+
+    const vi = ['https://telegra.ph/file/19631e87aca87dbcacdc6.mp4',
+'https://telegra.ph/file/19631e87aca87dbcacdc6.mp4',
+'https://telegra.ph/file/19631e87aca87dbcacdc6.mp4']
+    await conn.sendFile(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true} m, null, rcanal)
 
   } catch (e) {
     conn.reply(m.chat, 'Lo sentimos, el menú tiene un error.', m)
