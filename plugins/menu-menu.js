0f03c9d181b6d78039dd73e9fa4716693803cc36
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-07-12 02:34:33 -0400

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -156,7 +156,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     
-    let pp = './media/menus/Menu2.jpg'
+    let pp = 'https://telegra.ph/file/19631e87aca87dbcacdc6.mp4'
     await conn.sendFile(m.chat, pp, 'thumbnail.jpg', text.trim(), m, null, rcanal)
 
   } catch (e) {
