deffba8b343fa1329f1d8e71bf8868dfdecfce0e
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-07-25 22:24:20 -0400

    Update jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner }) => 
 ///  let parent = args[0] && args[0] == 'plz' ? _conn : await global.conn
 ///  if (!((args[0] && args[0] == 'plz') || (await global.conn).user.jid == _conn.user.jid)) {
 	////return m.reply(`Este comando solo puede ser usado en el bot principal! wa.me/${global.conn.user.jid.split`@`[0]}?text=${usedPrefix}code`)
-}
+//}
 
   async function serbot() {
 
