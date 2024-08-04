9a59dfb08f0d5b9db85269a69e8c9503da3b5fa6
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-08-04 11:13:15 -0400

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -3,14 +3,14 @@
 
 const handler = async (m, { text, conn, args, usedPrefix, command }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, 'Ingresa Un Link De Facebook', m);
+    return conn.reply(m.chat, 'Ingresa Un Link De Facebook', m, rcanal);
   }
 
   let res;
   try {
     res = await igdl(args[0]);
   } catch (error) {
-    return conn.reply(m.chat, 'Error al obtener datos. Verifica el enlace.', m);
+    return conn.reply(m.chat, 'Error al obtener datos. Verifica el enlace.', m, rcanal);
   }
 
   let result = res.data;
@@ -20,20 +20,20 @@ const handler = async (m, { text, conn, args, usedPrefix, command }) => {
 
   let data;
   try {
-    data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
+    data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "480p (SD)");
   } catch (error) {
-    return conn.reply(m.chat, 'Error al procesar los datos.', m);
+    return conn.reply(m.chat, 'Error al procesar los datos.', m, rcanal);
   }
 
   if (!data) {
-    return conn.reply(m.chat, 'No se encontró una resolución adecuada.', m);
+    return conn.reply(m.chat, 'No se encontró una resolución adecuada.', m, rcanal);
   }
 
   let video = data.url;
   try {
     await conn.sendMessage(m.chat, { video: { url: video }, caption: null, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m });
   } catch (error) {
-    return conn.reply(m.chat, 'Error al enviar el video.', m);
+    return conn.reply(m.chat, 'Error al enviar el video.', m, rcanal);
   }
 };
 
