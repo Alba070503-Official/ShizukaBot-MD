5e72526368b44c7f380991bdd7158a50c1119a60
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-08-10 07:40:05 -0400

    Update _autodetec.js

--- plugins/_autodetec.js
+++ plugins/_autodetec.js
@@ -1,93 +1,70 @@
-let WAMessageStubType = (await import(global.baileys)).default
-import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs';
-import path from 'path';
-import '../plugins/_content.js'
+import {WAMessageStubType} from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
+import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs'
+import path from 'path'
 
-let handler = m => m
-handler.before = async function (m, { conn, participants, groupMetadata }) {
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-    if (!m.messageStubType || !m.isGroup) return
+export async function before(m, { conn, participants, groupMetadata}) {
+if (!m.messageStubType || !m.isGroup) return
+let usuario = `@${m.sender.split`@`[0]}`
+const groupName = (await conn.groupMetadata(m.chat)).subject
+const groupAdmins = participants.filter((p) => p.admin)
 
-    let usuario = `@${m.sender.split`@`[0]}`
-    let chat = global.db.data.chats[m.chat]
-    let users = participants.map(u => conn.decodeJid(u.id))
-    const groupAdmins = participants.filter(p => p.admin)
-    const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
+let pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/3a8dfff34b09754dc30ca.jpg')
+const img = await (await fetch(pp)).buffer()
+const chat = global.db.data.chats[m.chat]
+const mentionsString = [m.sender, m.messageStubParameters[0], ...groupAdmins.map((v) => v.id)]
+const mentionsContentM = [m.sender, m.messageStubParameters[0]]
 
-    if (chat.detect && m.messageStubType == 21) {
-        await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoAG']() + mid.smsAutodetec1(usuario, m), mentions: [m.sender], mentions: [...groupAdmins.map(v => v.id)] }, { quoted: fkontak })   
-    } else if (chat.detect && m.messageStubType == 22) {
-        await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoIIG']() + mid.smsAutodetec2(usuario, groupMetadata), mentions: [m.sender] }, { quoted: fkontak })  
-    } else if (chat.detect && m.messageStubType == 23) {
-        await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoIIG']() + mid.smsAutodetec5(groupMetadata, usuario), mentions: [m.sender] }, { quoted: fkontak }) 
-    } else if (chat.detect && m.messageStubType == 24) {
-        await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoIIG']() + mid.smsAutodetec3(usuario, m), mentions: [m.sender] }, { quoted: fkontak }) 
-    } else if (chat.detect && m.messageStubType == 25) {
-        await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoIIG']() + mid.smsAutodetec4(usuario, m, groupMetadata), mentions: [m.sender] }, { quoted: fkontak })
-    } else if (chat.detect && m.messageStubType == 26) {
-        await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoIIG']() + mid.smsAutodetec6(m), mentions: [m.sender] }, { quoted: fkontak })
-    } else if (chat.detect && m.messageStubType == 29) {
-        let mentioned1 = m.messageStubParameters[0].split('@')[0];
-        let sender1 = m.sender.split('@')[0];
-        let txt1 = `🌸 *Nuevo admin*\n\n`;
-        txt1 += `Nombre: @${mentioned1}\n`;
-        txt1 += `Le otorgó admin: @${sender1}`;
-        
-        await conn.sendMessage(m.chat, {
-    text: txt1,
-    mentions: [`${mentioned1}@s.whatsapp.net`, `${sender1}@s.whatsapp.net`],
-    contextInfo: {
-        mentionedJid: [`${mentioned1}@s.whatsapp.net`, `${sender1}@s.whatsapp.net`],
-        externalAdReply: {
-            showAdAttribution: true,
-            containsAutoReply: true,
-            renderLargerThumbnail: true,
-            title: `ShizukaBot-MD Powered SpaceNigth Team `,
-            body: `*ੈ✩‧₊˚ 『SpaceNight Team』 *ੈ✩‧₊˚`,
-            mediaType: 1,
-            thumbnail: `https://telegra.ph/file/2828af24b9bd004bad80c.jpg`,
-            mediaUrl: `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`,
-            sourceUrl: `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`
-        }
-    }
-});
-    } else if (chat.detect && m.messageStubType == 30) {
-        let mentioned2 = m.messageStubParameters[0].split('@')[0];
-        let sender2 = m.sender.split('@')[0];
-        let txt2 = `🌸 *Un admin menos*\n\n`;
-        txt2 += `Nombre: @${mentioned2}\n`;
-        txt2 += `Le quitó admin: @${sender2}`;
-        
-        await conn.sendMessage(m.chat, {
-    text: txt2,
-    mentions: [`${mentioned2}@s.whatsapp.net`, `${sender2}@s.whatsapp.net`],
-    contextInfo: {
-        mentionedJid: [`${mentioned2}@s.whatsapp.net`, `${sender2}@s.whatsapp.net`],
-        "externalAdReply": {
-            "showAdAttribution": true,
-            "containsAutoReply": true,
-            "renderLargerThumbnail": true,
-            "title": `ShizukaBot-MD Powered SpaceNigth Team `,
-            "body": `*ੈ✩‧₊˚ 『SpaceNight Team』 *ੈ✩‧₊˚`,
-            "containsAutoReply": true,
-            "mediaType": 1,
-            "thumbnail": `https://telegra.ph/file/2828af24b9bd004bad80c.jpg`,
-            "mediaUrl": `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`,
-            "sourceUrl": `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`
-        }
-    }
-});
-    } else if (chat.detect && m.messageStubType == 72) {
-        await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoIIG']() + mid.smsAutodetec9(usuario, m), mentions: [m.sender] }, { quoted: fkontak })
-    } else if (chat.detect && m.messageStubType == 123) {
-        await this.sendMessage(m.chat, { text: lenguajeGB['smsAvisoIIG']() + mid.smsAutodetec10(usuario, m), mentions: [m.sender] }, { quoted: fkontak })
-    } else {
-        //console.log({
-        //    messageStubType: m.messageStubType,
-        //    messageStubParameters: m.messageStubParameters,
-        //    type: WAMessageStubType[m.messageStubType], 
-        //})
-    }
-}
+/*if (chat.detect && m.messageStubType == 2) {
+const chatId = m.isGroup ? m.chat : m.sender;
+const uniqid = chatId.split('@')[0];
+const sessionPath = './YoshiSession/';
+const files = await fs.readdir(sessionPath);
+let filesDeleted = 0;
+for (const file of files) {
+if (file.includes(uniqid)) {
+await fs.unlink(path.join(sessionPath, file));
+filesDeleted++;
+console.log(`⚠️ Eliminacion YoshiSession (PreKey) que provocan el undefined el chat`)}}}*/
 
-export default handler
+if (chat.detect && m.messageStubType == 21) {
+await this.sendMessage(m.chat, { text: `🍬 ${usuario} *Ha cambiado el nombre del grupo*`, mentions: [m.sender], mentions: (await conn.groupMetadata(m.chat)).participants.map(v => v.id) }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100}) 
+
+} else if (chat.detect && m.messageStubType == 22) {
+await this.sendMessage(m.chat, { text: `🍒 ${usuario} *Ha cambiado la imágen del grupo*`, mentions: [m.sender] }, { quoted: fliveLoc, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100}) 
+
+} else if (chat.detect && m.messageStubType == 24) {
+await this.sendMessage(m.chat, { text: `🍒 ${usuario} *Ha modificado la descripción!*\n\nNueva descripción:\n\n${m.messageStubParameters[0]}`, mentions: [m.sender] }, { quoted: fliveLoc, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+
+} else if (chat.detect && m.messageStubType == 25) {
+await this.sendMessage(m.chat, { text: `🫐 *Ahora ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} pueden editar la información del grupo*`, mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+
+} else if (chat.detect && m.messageStubType == 26) {
+await this.sendMessage(m.chat, { text: `🫐 *El grupo ha sido ${m.messageStubParameters[0] == 'on' ? 'cerrado' : 'abierto'}*\n\n${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} pueden enviar mensajes`, mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+
+} else if (chat.detect && m.messageStubType == 29) {
+let txt1 = `🌸 *Nuevo admin*\n\n`
+txt1 += `Nombre: @${m.messageStubParameters[0].split`@`[0]}\n`
+txt1 += `Le otorgó admin: @${m.sender.split`@`[0]}`
+
+await conn.sendMessage(m.chat, {text: txt1, mentions: [...txt1.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: { mentionedJid: [...txt1.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.packname, "body": `*ੈ✩‧₊˚ 『SpaceNight Team』 *ੈ✩‧₊˚`, "containsAutoReply": true, "mediaType": 1, "thumbnail": img, "mediaUrl": `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`, "sourceUrl": `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`}}})
+
+} else if (chat.detect && m.messageStubType == 30) {
+let txt2 = `🌸 *Un admin menos*\n\n`
+txt2 += `Nombre: @${m.messageStubParameters[0].split`@`[0]}\n`
+txt2 += `Le quitó admin: @${m.sender.split`@`[0]}`
+
+await conn.sendMessage(m.chat, {text: txt2, mentions: [...txt2.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: { mentionedJid: [...txt2.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.packname, "body": `*ੈ✩‧₊˚ 『SpaceNight Team』 *ੈ✩‧₊˚ `, "containsAutoReply": true, "mediaType": 1, "thumbnail": img, "mediaUrl": `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`, "sourceUrl": `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`}}})
+
+} else if (chat.detect && m.messageStubType == 72) {
+await this.sendMessage(m.chat, { text: `🫐 ${usuario} *Cambió la duración de mensajes temporales a @${m.messageStubParameters[0]}*`, mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+
+} else if (chat.detect && m.messageStubType == 123) {
+await this.sendMessage(m.chat, { text: `🍬 ${usuario} *Desactivó los mensajes temporales*`, mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+} else {
+console.log({messageStubType: m.messageStubType,
+messageStubParameters: m.messageStubParameters,
+type: WAMessageStubType[m.messageStubType], 
+})}}
