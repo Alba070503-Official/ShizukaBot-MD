7c1af2d11e78707fd20a3d01c95a2f68aa9a0428
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-10-02 01:43:56 -0400

    Update Prueba124.js

--- plugins/Prueba124.js
+++ plugins/Prueba124.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
         const messageText = `Hola Estela\n\nHay algo que llevo guardando en mi corazón desde hace tiempo, algo que, a pesar de lo mucho que he intentado poner en palabras, siempre parece escaparse de ellas. Pero hoy, quiero ser valiente y decirte lo que siento\n\nDesde el primer momento en que nuestras miradas se cruzaron, sentí una conexión especial. Con cada conversación, cada sonrisa que compartimos, me he dado cuenta de que ocupas un lugar muy especial en mi vida. Me haces querer ser mejor, y contigo, todo parece más fácil, más brillante.\n\nHoy quiero confesarte que me gustas. Me gustas de una manera que ni siquiera sabía que era posible. ¿Quieres salir conmigo?\n\nPresiona "Sí" o "No" abajo para responder.`;
 
         await conn.sendButton(m.chat, messageText, 'Declaración', imageUrl, [
-            ['Sí Acepto Salir Contigo', `${usedPrefix}menu`],
+            ['Sí Acepto Salir Contigo', `${usedPrefix}Si`],
             ['No Lo Siento Mucho', `${usedPrefix}no`]
         ], m);
     }
