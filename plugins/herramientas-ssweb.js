d20e8dfba96919a547105041c3029b07620b5457
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-10-13 23:18:25 -0400

    Update herramientas-ssweb.js

--- plugins/herramientas-ssweb.js
+++ plugins/herramientas-ssweb.js
@@ -1,63 +1,21 @@
-import fetch from 'node-fetch' 
-import axios from "axios"
-const handler = async (m, {conn, text, args, usedPrefix, isPrems}) => {   
-if (!args[0]) return conn.reply(m.chat, '*Por favor ingresa una url de la página a la que se le tomará captura 🔎*', m)  
-let user = global.db.data.users[m.sender]
-try{
-     await conn.sendMessage(m.chat, { image: { url: `https://image.thum.io/get/fullpage/${args[0]}` }, caption: `Tu imagen 📷` }, { quoted: m })
-}
-catch{
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command, args }) => {
+if (!args[0]) return conn.reply(m.chat, '⚠️ *Ingrese el Link de una página.*', m, rcanal)
 try {
-let krt = await ssweb(args[0])
-/*let calidad, webIMG 
-try {  
-if (!user.premiumTime) {
-calidad = '1280x720' //HD
-webIMG = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=${calidad}`
-await conn.sendMessage(m.chat, { image: { url: krt.result }, caption: `🎟️ *PREMIUM:* ${user.premiumTime > 0 ? '✅' : '❌'}\n🪄 *CALIDAD:* \`\`\`(${calidad}) HD\`\`\`\n\n👑 _Para una imagen en *4K*, adquiera un pase usando *${usedPrefix}pase premium*_` }, { quoted: m }) 
-} else {
-calidad = '3840x2160' //4K
-webIMG = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=${calidad}`*/
-await conn.sendMessage(m.chat, { image: { url: krt.result }, caption: `Tu imagen 📷` }, { quoted: m }) 
-//}
-} catch { 
-m.reply("Error.")
-}}}
-handler.command = /^ss(web)?f?$/i  
+await m.react(rwait)
+conn.reply(m.chat, '🚀 Buscando su información....', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: dev,
+previewType: 0, thumbnail: icons, 
+sourceUrl: channel }}})
+let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
+conn.sendFile(m.chat, ss, 'error.png', args[0], fkontak)
+await m.react(done)
+} catch {
+return conn.reply(m.chat, '🚩 Ocurrió un error.', m, fake)
+await m.react(error)}}
+handler.help = ['ssweb', 'ss']
+handler.tags = ['tools']
+handler.command = ['ssweb', 'ss']
 export default handler
-
-async function ssweb(url, device = 'desktop') {
-     return new Promise((resolve, reject) => {
-          const base = 'https://www.screenshotmachine.com'
-          const param = {
-            url: url,
-            device: device,
-            cacheLimit: 0
-          }
-          axios({url: base + '/capture.php',
-               method: 'POST',
-               data: new URLSearchParams(Object.entries(param)),
-               headers: {
-                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
-               }
-          }).then((data) => {
-               const cookies = data.headers['set-cookie']
-               if (data.data.status == 'success') {
-                    axios.get(base + '/' + data.data.link, {
-                         headers: {
-                              'cookie': cookies.join('')
-                         },
-                         responseType: 'arraybuffer'
-                    }).then(({ data }) => {
-                        result = {
-                            status: 200,
-                            result: data
-                        }
-                         resolve(result)
-                    })
-               } else {
-                    reject({ status: 404, statuses: `Link Error`, message: data.data })
-               }
-          }).catch(reject)
-     })
-}
