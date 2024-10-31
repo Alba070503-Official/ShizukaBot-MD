403ba97bdc7d9018ac54888538a88a9b8452ba46
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-30 20:56:55 -0500

    Actualizar rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -10,7 +10,7 @@ externalAdReply: {
 title: "【 🔔 Notificación General 🔔 】",
 body: '🐢 Un usuario anuló su registro.',
 thumbnailUrl: fotoperfil,
-sourceUrl: redes,
+sourceUrl: canales,
 mediaType: 1,
 showAdAttribution: false,
 renderLargerThumbnail: false
