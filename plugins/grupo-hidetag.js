fa81c9ee6dd7cf7bb27fe45f24e1f83d33b76bc1
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-12-01 11:15:46 -0400

    Update grupo-hidetag.js

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -1,17 +1,24 @@
-import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
 import * as fs from 'fs';
-const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
+
+const handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
   try {
     const users = participants.map((u) => conn.decodeJid(u.id));
     const q = m.quoted ? m.quoted : m || m.text || m.sender;
     const c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender;
-    const msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, {[m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : {text: '' || c}}, {quoted: m, userJid: conn.user.id}), text || q.text, conn.user.jid, {mentions: users});
-    await conn.relayMessage(m.chat, msg.message, {messageId: msg.key.id});
+    const msg = conn.cMod(
+      m.chat,
+      generateWAMessageFromContent(
+        m.chat,
+        { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c } },
+        { quoted: m, userJid: conn.user.id }
+      ),
+      text || q.text,
+      conn.user.jid,
+      { mentions: users }
+    );
+    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
   } catch {
-    /**
-[ By @NeKosmic || https://github.com/NeKosmic/ ]
-**/
-
     const users = participants.map((u) => conn.decodeJid(u.id));
     const quoted = m.quoted ? m.quoted : m;
     const mime = (quoted.msg || quoted).mimetype || '';
@@ -20,24 +27,44 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     const masss = more.repeat(850);
     const htextos = `${text ? text : '*Hola :D*'}`;
     if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
-      var mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, {image: mediax, mentions: users, caption: htextos, mentions: users}, {quoted: m});
+      const mediax = await quoted.download?.();
+      conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos }, { quoted: m });
     } else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
-      var mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, {video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos}, {quoted: m});
+      const mediax = await quoted.download?.();
+      conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos }, { quoted: m });
     } else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
-      var mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, {audio: mediax, mentions: users, mimetype: 'audio/mpeg', fileName: `Hidetag.mp3`}, {quoted: m});
+      const mediax = await quoted.download?.();
+      conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mpeg', fileName: `Hidetag.mp3` }, { quoted: m });
     } else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
-      var mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, {quoted: m});
+      const mediax = await quoted.download?.();
+      conn.sendMessage(m.chat, { sticker: mediax, mentions: users }, { quoted: m });
     } else {
-      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: imagen1, sourceUrl: 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}}}}}, {});
+      await conn.relayMessage(
+        m.chat,
+        {
+          extendedTextMessage: {
+            text: `${masss}\n${htextos}\n`,
+            contextInfo: {
+              mentionedJid: users,
+              externalAdReply: {
+                thumbnail: imagen1,
+                sourceUrl: 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+              },
+            },
+          },
+        },
+        {}
+      );
     }
   }
 };
+
+// Configuración del comando
 handler.command = /^(hidetag|notificar|notify)$/i;
 handler.group = true;
-handler.owner = true;
+
+// Permitir uso por admin o dueño del bot
 handler.admin = true;
+handler.owner = true;
+
 export default handler;
